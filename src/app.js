435b093f8ca5ee4e459efb4734da3a15cfc44268
kostyan <kozzztya@gmail.com>
2016-04-07 21:19:55 +0300

    Remove /public dir

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const serveStatic = require('feathers').static;
-const favicon = require('serve-favicon');
 const compress = require('compression');
 const cors = require('cors');
 const feathers = require('feathers');
@@ -18,8 +16,6 @@ app.configure(configuration(path.join(__dirname, '..')));
 app.use(compress())
   .options('*', cors())
   .use(cors())
-  .use(favicon(path.join(app.get('public'), 'favicon.ico')))
-  .use('/', serveStatic(app.get('public')))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .configure(hooks())
