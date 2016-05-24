f459106f5053e0930cc61625b42bb463e0bf91c7
kostyan <kozzztya@gmail.com>
2016-05-24 16:26:07 +0300

    Send a push notification to a recipient specific token

---
+++ src/services/provider/routes/push-notify-token.js
@@ -0,0 +1,9 @@
+const Notification = require('../../notification/model');
+const pushHelper = require('../../../helpers/push-send');
+
+module.exports = async({ token, message, payload, platform, deviceId }) => {
+  const push = { address: token, message, payload, platform, deviceId };
+  await Notification.create(push);
+
+  return await pushHelper.send(push);
+};
