1afcbcbff95a0f75b9a0f03dce51eae6fb62b39b
kostyan <kozzztya@gmail.com>
2016-06-15 16:20:59 +0300

    Add stub for setEndpointAttributes

--- test/mocks/aws.stub.js
+++ test/mocks/aws.stub.js
@@ -3,13 +3,17 @@ const sinon = require('sinon');
 let createEndpointStub;
 let publishStub;
 let awsStub;
+let setEndpointAttributesStub;
 
 function register() {
   createEndpointStub = sinon.stub().returns({ EndpointArn: '1' });
   publishStub = sinon.stub().returns({ MessageId: '2' });
+  setEndpointAttributesStub = sinon.stub();
+
   awsStub = sinon.stub(require('aws-sdk'), 'SNS', () => ({
     createPlatformEndpointAsync: createEndpointStub,
-    publishAsync: publishStub
+    publishAsync: publishStub,
+    setEndpointAttributesAsync: setEndpointAttributesStub
   }));
 
   return { awsStub, createEndpointStub, publishStub };
