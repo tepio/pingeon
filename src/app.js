f01abe7476072e1385e29edd850e8c7a50604f77
Constantine ZV <kozzztya@gmail.com>
2016-08-19 20:18:14 +0300

    Add getsentry (#91)
    Story #128573471

--- src/app.js
+++ src/app.js
@@ -6,17 +6,19 @@ const rest = require('feathers-rest');
 const bodyParser = require('body-parser');
 const middleware = require('./middleware');
 const services = require('./services');
+const sentry = require('./helpers/sentry');
 
 const app = feathers();
-
 app.use(compress())
   .options('*', cors())
   .use(cors())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
+  .use(sentry.requestHandler)
   .configure(hooks())
   .configure(rest())
   .configure(services)
-  .configure(middleware);
+  .configure(middleware)
+  .use(sentry.errorHandler);
 
 module.exports = app;
