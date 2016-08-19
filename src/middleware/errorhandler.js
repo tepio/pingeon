1bad0bab431f0ab87e4ca8e41fbf83ab74ac3623
kostyan <kozzztya@gmail.com>
2016-08-19 20:17:34 +0300

    Add getsentry
    Story #128573471

--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -1,6 +1,6 @@
 module.exports = () => (err, req, res, next) => {
-  if (!err) next();
-  if (!err.toJSON) next(err);
+  if (!err) return next();
+  if (!err.toJSON) return next(err);
 
   const error = err.toJSON();
   return res.status(error.code).send({ error });
