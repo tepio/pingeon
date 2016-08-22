f9a068866e9b5bd67af7c333b907c05f8cba1e89
kostyan <kozzztya@gmail.com>
2016-08-22 15:50:47 +0300

    Save sent/failed status of push notification
    Story #128754987

--- src/helpers/push-receive-status.js
+++ src/helpers/push-receive-status.js
@@ -1,56 +1,18 @@
 const debug = require('debug')('app:helpers:push-receive-status');
-const AWS = require('aws-sdk');
-const async = require('async');
-const extend = require('deep-extend');
-const _ = require('lodash');
+const Notification = require('../services/notification/model');
 
-const RECEIVE_STATUS_INTERVAL = 100;
-const RECEIVE_STATUS_TIMES = 3;
+function saveSuccessful({ platformApplicationArn, providerMessageId, platform, token, message, payload }) {
+  const result = { sendDate: new Date(), platformApplicationArn, providerMessageId, platform, token, message, payload };
+  Notification.create(result);
+  debug('push sent', result);
 
-const config = require('./config');
-const { key, secret, region } = config.get('push');
-
-AWS.config.update({ accessKeyId: key, secretAccessKey: secret, region });
-const pushLogs = new AWS.CloudWatchLogs();
-const awsUtils = require('../helpers/aws-utils');
-
-function get({ providerMessageId, platformApplicationArn }) {
-  return new Promise(resolve => {
-    const logGroupName = awsUtils.getLogGroup(platformApplicationArn);
-
-    async.retry({ times: RECEIVE_STATUS_TIMES, interval: RECEIVE_STATUS_INTERVAL }, (cb) => {
-
-      const params = {
-        filterPattern: `{ ($.notification.messageId = "${providerMessageId}") }`,
-        limit: 1,
-        startTime: 0
-      };
-
-      async.parallel({
-        success: (cb) => pushLogs.filterLogEvents(extend(params, { logGroupName }),
-          (err, res) => {
-            if (err) debug(err);
-            cb(null, res);
-          }),
-        failure: (cb) => pushLogs.filterLogEvents(extend(params, { logGroupName: logGroupName + '/Failure' }),
-          (err, res) => {
-            if (err) debug(err);
-            cb(null, res);
-          })
-      }, (err, res) => {
-        if (err) return cb(err);
-
-        const success = _.get(res, 'success.events[0]');
-        const failure = _.get(res, 'failure.events[0]');
-
-        const receiveStatus = success || failure;
-        if (!receiveStatus) return cb('retry');
-
-        return resolve({ received: !!success, receiveStatus });
-      });
-    }, () => {
-    });
-  });
+  return result;
 }
 
-module.exports = { get };
+function saveFailed({ platform, token, message, payload, error }) {
+  const failedPush = { platform, token, message, payload, error };
+  Notification.create(failedPush);
+  debug('push sent', failedPush);
+}
+
+module.exports = { saveFailed, saveSuccessful };
--- test/push-send-helper.test.js
+++ test/push-send-helper.test.js
@@ -3,13 +3,16 @@ require('./test-env');
 const { createEndpointStub, publishStub } = mocks.awsStub.getStubs();
 
 const pushProvider = require('../src/helpers/push-send');
-const pushReceiveStatus = require('../src/helpers/push-receive-status');
-const { queueClient, PUSH_SENT_FAIL } = require('../src/helpers/queue');
+const pushRegister = require('../src/services/recipient-profile/routes/push-register');
+const Notification = require('../src/services/notification/model');
+const RecipientProfile = require('../src/services/recipient-profile/model');
+
 const platform = 'android';
 const token = String(new Date());
-const message = 'Hello!';
+const message = String(new Date());
+const error = { message: String(new Date()) };
 const payload = { badge: 5 };
-const error = { message: 'Uh oh!' };
+const EndpointArn = '1';
 
 describe('Push', () => {
 
@@ -19,16 +22,16 @@ describe('Push', () => {
   describe('send', () => {
 
     describe('success', () => {
-      
+
       before(() => {
         createEndpointStub.returns({ EndpointArn: '1' });
         publishStub.returns({ MessageId: '2' });
       });
 
-      before(() => {
+      it('should be sent', () => {
         return pushProvider
           .send({ platform, token, message, payload })
-          .then(res => {
+          .then(async res => {
             assert.equal(res.platform, platform);
             assert.equal(res.message, message);
             assert.equal(res.token, token);
@@ -36,19 +39,18 @@ describe('Push', () => {
             assert.ok(res.platformApplicationArn);
             assert.ok(res.providerMessageId);
             assert.ok(res.sendDate);
-          });
-      });
+            assert.ok(ctx.pushSendSpy.called);
 
-      it('should be sent', () => {
-        assert(ctx.pushSendSpy.called);
+            assert.ok(await Notification.findOne({ providerMessageId: res.providerMessageId }));
+          });
       });
 
     });
 
-    describe.skip('fail', () => {
+    describe('fail', () => {
 
       before(() => {
-        createEndpointStub.returns({ EndpointArn: '1' });
+        createEndpointStub.returns({ EndpointArn });
         publishStub.throws(error);
       });
 
@@ -56,30 +58,40 @@ describe('Push', () => {
         pushProvider.send({ platform, token, message, payload });
       });
 
-      it('should be fail', done => {
-        queueClient(PUSH_SENT_FAIL).subscribe(res => {
-          assert.deepEqual(res, error);
-          done();
-        });
+      it('should be fail', async() => {
+        assert.ok(await Notification.findOne({ error }));
       });
 
     });
 
   });
 
-  describe.skip('receive status', function () {
-    this.timeout(10000);
+  describe('old token', () => {
 
-    it('should return status', () => {
-      return pushReceiveStatus
-        .get({
-          providerMessageId: '49f7e242-2412-594c-86e8-adcdd25dbba1',
-          platformApplicationArn: 'arn:aws:sns:us-east-1:093525834944:app/APNS_SANDBOX/outfit-development'
-        })
-        .then(res => {
-          assert.ok(res.received);
-          assert.ok(res.receiveStatus);
-        });
+    const oldTokenError = { message: 'Invalid parameter: This endpoint is already registered with a different token.' };
+    const oldToken = String(new Date());
+
+    before(async () => {
+      await pushRegister({ token: oldToken, deviceId: 'some', recipientId: 'some' });
+    });
+
+    before(async() => {
+      const res = await RecipientProfile.findOne({ token: oldToken });
+      assert.ok(res);
+    });
+    
+    before(() => {
+      createEndpointStub.returns({ EndpointArn });
+      publishStub.throws(oldTokenError);
+    });
+
+    before(() => {
+      pushProvider.send({ platform, token: oldToken, message, payload });
+    });
+
+    it('should be no old device token', async() => {
+      const res = await RecipientProfile.findOne({ token: oldToken });
+      assert.ok(!res);
     });
 
   });
