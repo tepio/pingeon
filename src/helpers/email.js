005b94f2c28745f97df304b02b916640726324b9
kostyan <kozzztya@gmail.com>
2016-10-03 17:04:46 +0300

    Switch pingeon email service to Postmark

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,41 +1,40 @@
 const debug = require('debug')('app:email');
 
-const mandrill = require('mandrill-api/mandrill');
-const { getVars, getTo } = require('./mandrill-utils');
 const config = require('./config');
 const emailConfig = config.get('email');
-const mandrillClient = new mandrill.Mandrill(emailConfig.key);
+const { promisifyAll } = require('bluebird');
+const postmark = require('postmark');
+const client = promisifyAll(new postmark.Client(emailConfig.key));
 
-const send = ({ email, to, cc, bcc, subject, message, template, vars }) => new Promise((resolve, reject) => {
-  const mandrillMessage = {
-    subject,
-    to: getTo({ email, to, cc, bcc }),
-    from_email: emailConfig.from,
-    merge_vars: [{
-      rcpt: email,
-      vars: getVars(vars)
-    }]
-  };
+const send = ({ email, to, cc, bcc, subject, message, template, vars }) => {
 
-  if (template) {
-    return mandrillClient.messages.sendTemplate({
-      template_name: template,
-      template_content: [{}],
-      message: mandrillMessage,
-      async: false
-    }, resolve, reject);
+  try {
+    const options = {
+      From: emailConfig.from,
+      To: email || to,
+      Cc: cc,
+      Bcc: bcc
+    };
+
+    if (template) {
+      return client.sendEmailWithTemplateAsync({
+        TemplateId: template,
+        TemplateModel: vars,
+        ...options
+      });
+    }
+
+    return client.sendEmailAsync({
+      TextBody: message,
+      Subject: subject,
+      ...options
+    });
+
+  } catch (err) {
+    debug(err);
+    return err;
   }
 
-  return mandrillClient.messages.send({
-    message: { ...mandrillMessage, text: message },
-    async: false
-  }, res => {
-    debug('sent', res);
-    resolve(res);
-  }, err => {
-    debug('error', err);
-    reject(err);
-  });
-});
+};
 
 module.exports = { send };
--- test/email.test.js
+++ test/email.test.js
@@ -1,20 +1,49 @@
 require('./test-env');
 
+const assert = require('assert');
 const emailProvider = require('../src/helpers/email');
 const email = 'kozzztya@gmail.com';
-const template = 'thank-you-registering';
-const vars = { completeregistration: 'some' };
+const cc = email;
+const bcc = email;
+const template = '973317';
+const vars = {
+  product_name: 'Pingeon',
+  name: 'Vasya',
+  action_url: 'http://google.com',
+  sender_name: 'Petya',
+  product_address_line1: 'http://google.com',
+  product_address_line2: 'http://google.com'
+};
+const message = 'Hello!';
+const subject = 'Test!';
 
 describe('Email send', function () {
   this.timeout(100000);
 
-  it('should be sent', done => {
-    return emailProvider
-      .send({ email, template, vars })
-      .then(res => {
-        assert.ok(res);
-        done();
-      });
+  describe('message', () => {
+
+    it('should be sent', done => {
+      return emailProvider
+        .send({ email, message, subject, cc, bcc })
+        .then(res => {
+          assert.ok(res);
+          done();
+        });
+    });
+
+  });
+
+  describe('template', () => {
+
+    it('should be sent', done => {
+      return emailProvider
+        .send({ email, template, vars, cc, bcc })
+        .then(res => {
+          assert.ok(res);
+          done();
+        });
+    });
+
   });
 
 });
