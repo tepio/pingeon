a649bc1674e495c085f8cb546c6e1c4e021497a5
kostyan <kozzztya@gmail.com>
2016-10-13 19:50:58 +0300

    tes jenkins

--- deploy.js
+++ deploy.js
@@ -5,3 +5,4 @@ const branch = _.trim(process.env.GIT_BRANCH, 'origin/');
 
 if (branch === 'dev' || /build-/.test(branch)) sh.exec('npm run deploy:staging');
 if (branch === 'master' || /release-/.test(branch)) sh.exec('npm run deploy:production');
+
