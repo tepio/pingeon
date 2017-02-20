6e05f937a10d190a970689e7d17ab3beb84fb645
kostyan <kozzztya@gmail.com>
2017-02-20 18:01:39 +0200

    Refactor tests in docker

--- test/docker/run-tests.sh
+++ test/docker/run-tests.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# If script wasn't run in Jenkins set default tag
-if [ -z "$BUILD_TAG" ]
-then
-  BUILD_TAG="test-1"
-fi
-
-# Prepare project and container names for proper status check
-PROJECT=`echo $BUILD_TAG | sed "s/\-//g"`
-CONTAINER=$PROJECT"_app_1"
+set -e
 
 # Build environment and run ci scripts
-docker-compose -f test/docker/docker-compose.yml -p $PROJECT up --abort-on-container-exit --build
+docker-compose -f test/docker/docker-compose.yml up -d --build
 
-# Status check
-exit $(docker wait $CONTAINER)
+# See the output from the scripts
+docker attach app
+
+# Clean everything up
+docker-compose -f test/docker/docker-compose.yml kill
+docker-compose -f test/docker/docker-compose.yml rm -f
