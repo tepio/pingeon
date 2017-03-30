3b9eb91969ae4bff269b868ace69a936a3664709
kostyan <kozzztya@gmail.com>
2017-03-30 16:46:59 +0300

    Fix tests

--- test/docker/run-tests.sh
+++ test/docker/run-tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-set -e
+# If script wasn't run in Jenkins set default tag
+if [ -z "$JOB_NAME" ]
+then
+  JOB_NAME="test-1"
+fi
+
+# Prepare project and container names for proper status check
+PROJECT=`echo $JOB_NAME | sed "s/\-//g"`
+CONTAINER=$PROJECT"_app_1"
 
 # Build environment and run ci scripts
-docker-compose -f test/docker/docker-compose.yml up -d --build
+docker-compose -f test/docker/docker-compose.yml -p $PROJECT up -d --build --force-recreate
 
-# See the output from the scripts
-docker attach app
+# Check status
+docker attach $CONTAINER
 
 # Clean everything up
-docker-compose -f test/docker/docker-compose.yml kill
-docker-compose -f test/docker/docker-compose.yml rm -f
+docker-compose -f test/docker/docker-compose.yml -p $PROJECT kill
+docker-compose -f test/docker/docker-compose.yml -p $PROJECT rm -f
