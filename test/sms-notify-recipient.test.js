658bfc92e5f3adc9bcb50f205daa5edfaaa2e050
vanya andreychuk <vandreychyk@gmail.com>
2017-05-22 14:45:27 +0300

    - remove empty line

--- test/sms-notify-recipient.test.js
+++ test/sms-notify-recipient.test.js
@@ -5,7 +5,6 @@ const message = {
   recipientNumber: '+13346498383'
 };
 
-
 describe('Send SMS via TWILIO', () => {
   it('should receive status code 201', () => request
     .post('/provider/sms/recipient')
